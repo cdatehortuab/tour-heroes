36f01f3d555aa0b25b555790b16a3266d4e8a8d3
Cristian Atehortua <cristian25a32@gmail.com>
2019-04-18 13:31:32 -0500

    4: Routing - Added links to hero detail component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { HeroService } from '../../services/hero/hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -21,13 +20,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero) {
-    if (this.selectedHero === hero) {
-      this.selectedHero = null;
-    } else {
-      this.selectedHero = hero;
-    }
   }
 }
